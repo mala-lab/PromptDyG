718e814e57b0c50f06b501d22de789928c9b6175
GGA23 <136722638+GGA23@users.noreply.github.com>
2026-06-03 10:31:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
 python3 main.py --cfg ./replication_configs/reddit-body-gru-0.5.yaml
+
+#for epoch in '1' '5' '10' '20'
+#do
+#    for lr in '0.001' '0.005' '0.0005' '0.01'
+#    do
+#        for s in '1' '2' '3'
+#        do
+#            python3 main.py --cfg ./replication_configs/reddit-body-gru-0.5.yaml --seed $s --TTA_epochs $epoch --TTA_lr_feat $lr
+#        done    
+#    done
+#done
